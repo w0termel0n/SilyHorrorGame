9151ba101b6c25c0284f3aee20ee7c534c55f40f
benps <benpsnook@gmail.com>
2025-01-27 13:26:53 -0600

    Update

--- silygame/game.go
+++ silygame/game.go
@@ -1,12 +1,44 @@
 package silygame
 
+import (
+	//"fmt"
+	//"image/color"
+	"time"
+	//"github.com/hajimehoshi/ebiten/v2"
+	//"github.com/w0termel0n/SilyHorrorGame/assets"
+)
+
 const (
 	screen = 800
 	width  = 600
+
+	baseVelocity = 0.25
+	duration     = 15 * time.Second
 )
 
 type Game struct {
-	player       *Player
-	baseVelocity float64
-	velTimer     *Timer
+	//player       *Player
+	velocity float64
+	velTimer *Timer
 }
+
+func NewGame() *Game {
+	g := &Game{
+		velocity: baseVelocity,
+		velTimer: NewTimer(duration),
+	}
+
+	//g.player = NewPlayer(g)
+
+	return g
+}
+
+/*
+func (g *Game) Update() error {
+
+}
+
+func (g *Game) Draw(screen ebiten.Image) {
+
+}
+*/
